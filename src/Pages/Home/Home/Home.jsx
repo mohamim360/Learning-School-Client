6a727f33ee232f28fa51e07c92d7212154cc1d91
mohamim360 <mohamim360@gmail.com>
2023-06-13 22:54:00 +0600

    add Popular Classes Section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../Banner/Banner";
+import PopularClasses from "../Popular/PopularClasses";
+import Populerinstructor from "../Popular/Populerinstructor";
 
 
 const Home = () => {
   return (
     <div>
      <Banner></Banner>
+     <PopularClasses></PopularClasses>
+     <Populerinstructor></Populerinstructor>
     </div>
   );
 };
---
+++ src/Pages/Home/Popular/PopularClasses.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const PopularClasses = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const sortedData = data.sort((a, b) => a.availableSeats - b.availableSeats);
+        const topClasses = sortedData.slice(0, 6);
+        setData(topClasses);
+      });
+  }, []);
+
+  return (
+    <div className="mt-5">
+      <h2>Popular Classes</h2>
+      <div className="flex justify-between">
+        {data.map((item) => (
+          <div key={item.id} className="flex items-center">
+            <img src={item.classImage} alt={item.name} className="w-40 h-40 object-cover rounded" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularClasses;
