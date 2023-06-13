4c966ba0881ac4f5d03ab0b1c81ca367ff476e12
mohamim360 <mohamim360@gmail.com>
2023-06-13 22:54:43 +0600

    Popular Instructors Section

---
+++ src/Pages/Home/Popular/Populerinstructor.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+
+const Populerinstructor = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => {
+        const sortedData = data.sort((a, b) => a.availableSeats - b.availableSeats);
+        const topClasses = sortedData.slice(0, 6);
+        setData(topClasses);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Popular Instructors</h2>
+      <div className="flex justify-between">
+        {data.map(item => (
+          <div key={item.id} className="flex items-center">
+            <img src={item.instructorImage} alt={item.instructorName} className="w-40 h-40 object-cover rounded" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Populerinstructor;
