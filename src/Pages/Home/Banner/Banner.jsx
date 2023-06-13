262876281b47c23331776eb38a5a110f0c7ed018
mohamim360 <mohamim360@gmail.com>
2023-06-13 21:42:12 +0600

    added banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,30 +1,34 @@
-
+import { Fade } from "react-awesome-reveal";
+import AwesomeSlider from "react-awesome-slider";
+import 'react-awesome-slider/dist/styles.css';
 
 const Banner = () => {
   return (
-    <AwesomeSlider>
-    <div>
-      <Fade>
-        <img src="your-image-url-1" alt="Slider Image 1" />
-        <h2>Slider 1 Title</h2>
-        <p>Slider 1 Description</p>
-      </Fade>
+    <div className="relative pb-5">
+      <AwesomeSlider>
+        <div className="flex flex-col items-center justify-center h-64">
+          <Fade>
+            <img src="src/assets/download.jpeg" className="w-full h-full object-cover" alt="Slider Image 1" />
+            <h2 className="text-white">Slider 1 Title</h2>
+            <p className="text-white">Slider 1 Description</p>
+          </Fade>
+        </div>
+        <div className="flex flex-col items-center justify-center h-64">
+          <Fade>
+            <img src="src/assets/download (2).jpeg" className="w-full h-full object-cover" alt="Slider Image 2" />
+            <h2 className="text-white">Slider 2 Title</h2>
+            <p className="text-white">Slider 2 Description</p>
+          </Fade>
+        </div>
+        <div className="flex flex-col items-center justify-center h-64">
+          <Fade>
+            <img src="src/assets/download (1).jpeg" className="w-full h-full object-cover" alt="Slider Image 3" />
+            <h2 className="text-white">Slider 3 Title</h2>
+            <p className="text-white">Slider 3 Description</p>
+          </Fade>
+        </div>
+      </AwesomeSlider>
     </div>
-    <div>
-      <Fade>
-        <img src="your-image-url-2" alt="Slider Image 2" />
-        <h2>Slider 2 Title</h2>
-        <p>Slider 2 Description</p>
-      </Fade>
-    </div>
-    <div>
-      <Fade>
-        <img src="your-image-url-3" alt="Slider Image 3" />
-        <h2>Slider 3 Title</h2>
-        <p>Slider 3 Description</p>
-      </Fade>
-    </div>
-  </AwesomeSlider>
   );
 };
 
