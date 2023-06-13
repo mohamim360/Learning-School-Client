9fb6e80b0a96df2bf132d6bb080dbde3cd04978e
mohamim360 <mohamim360@gmail.com>
2023-06-13 20:53:50 +0600

    added navber

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
-import Navber from "../Pages/Shared/NavBer";
+
 import NavBer from "../Pages/Shared/NavBer";
 
 
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+
+
+const Banner = () => {
+  return (
+    <AwesomeSlider>
+    <div>
+      <Fade>
+        <img src="your-image-url-1" alt="Slider Image 1" />
+        <h2>Slider 1 Title</h2>
+        <p>Slider 1 Description</p>
+      </Fade>
+    </div>
+    <div>
+      <Fade>
+        <img src="your-image-url-2" alt="Slider Image 2" />
+        <h2>Slider 2 Title</h2>
+        <p>Slider 2 Description</p>
+      </Fade>
+    </div>
+    <div>
+      <Fade>
+        <img src="your-image-url-3" alt="Slider Image 3" />
+        <h2>Slider 3 Title</h2>
+        <p>Slider 3 Description</p>
+      </Fade>
+    </div>
+  </AwesomeSlider>
+  );
+};
+
+export default Banner;
