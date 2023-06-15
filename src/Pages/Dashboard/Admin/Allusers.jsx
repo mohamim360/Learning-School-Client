316ed6d6afc2a0c0f48cd2088af721087411dd29
mohamim360 <mohamim360@gmail.com>
2023-06-16 03:15:12 +0600

    added admin and allusers

--- src/Pages/Dashboard/Admin/Allusers.jsx
+++ src/Pages/Dashboard/Admin/Allusers.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const Allusers = () => {
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
+    return res.json();
+  });
+
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          alert("new admin is added");
+        }
+      });
+  };
+
   return (
     <div>
-      
+      <table className="min-w-full border-collapse">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">Name</th>
+            <th className="py-2 px-4 border-b">Email</th>
+            <th className="py-2 px-4 border-b">Role</th>
+            <th className="py-2 px-4 border-b"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td className="py-2 px-4 border-b">{user.name}</td>
+              <td className="py-2 px-4 border-b">{user.email}</td>
+              <td className="py-2 px-4 border-b">{user.role}</td>
+              <td className="py-2 px-4 border-b">
+                {user.role !== "admin" && (
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    disabled={user.role === "admin"}
+                    onClick={() => handleMakeAdmin(user)}
+                  >
+                    Make Admin
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
